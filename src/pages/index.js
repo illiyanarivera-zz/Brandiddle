9069a922693b4469afd2748905a35a6a178270df
illiyanarivera <imrivera1@loyola.edu>
2021-04-05 11:39:02 -0400

    updated index.js and database

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+
+// configure amplify
+import Amplify from '@aws-amplify/core';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+
 import { Helmet } from 'react-helmet';
+import awsconfig from '../aws-exports';
 import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
+Amplify.configure(awsconfig);
+
 export default () => {
   const { title, lang, description } = headData;
 
   return (
-    <>
+    <AmplifyAuthenticator>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{title || 'Gatsby Simplefolio'}</title>
@@ -17,6 +25,6 @@ export default () => {
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
       <App />
-    </>
+    </AmplifyAuthenticator>
   );
 };
