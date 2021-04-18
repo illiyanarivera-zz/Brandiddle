175eb5429461bc8f1874d53acf50229a14c31e81
illiyanarivera <imrivera1@loyola.edu>
2021-04-17 23:20:44 -0400

    updating the readme: now connected to the database, secure sign in and out, updated the website layout [organized the pages]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,28 +7,28 @@ import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { heroData, projectsData, aboutData, contactData, footerData } from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
-  const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
+  const [about, setAbout] = useState({});
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
-    setAbout({ ...aboutData });
     setProjects([...projectsData]);
+    setAbout({ ...aboutData });
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+    <PortfolioProvider value={{ hero, projects, about, contact, footer }}>
       <Hero />
-      <About />
       <Projects />
+      <About />
       <Contact />
       <Footer />
     </PortfolioProvider>
